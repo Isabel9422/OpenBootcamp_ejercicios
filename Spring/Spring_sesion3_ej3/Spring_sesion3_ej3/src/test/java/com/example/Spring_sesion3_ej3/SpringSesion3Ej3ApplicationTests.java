bfb764b5fc15f5953f174e4898ff8e5ce2189992
Isabel9422 <isabels94@hotmail.com>
2022-05-20 22:15:41 +0200

    Subiendo ejercicios de las sesiones 1,2,3 de Spring

---
+++ Spring/Spring_sesion3_ej3/Spring_sesion3_ej3/src/test/java/com/example/Spring_sesion3_ej3/SpringSesion3Ej3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_sesion3_ej3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSesion3Ej3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
