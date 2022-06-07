2aa0d2146ea8596c5aa5d25c8a9172cd443d7fd8
Isabel9422 <65084343+Isabel9422@users.noreply.github.com>
2022-06-07 11:36:50 +0200

    Delete Spring directory

--- Spring/Spring_sesion3_ej3/src/main/java/com/example/Spring_sesion3_ej3/PerroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Spring_sesion3_ej3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PerroRepository extends JpaRepository<Perro,Integer> {
-}
